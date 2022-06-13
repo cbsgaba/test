25429228aae12225ec11b39f5bf8fdc932bbd134
cbsgaba <cbsgaba@gmail.com>
2022-06-14 06:39:56 +0800

    v1.2 added iris image segmentation second half

--- Iris recognition system/main.m
+++ Iris recognition system/main.m
@@ -4,7 +4,18 @@ clear;
 workspace;
 %--------------------------------------------
 %image = imageacquisition();
-iris_image = imread('~/Documents/Github/git_workspace/test/MMU-Iris-Database/1/left/aeval1.bmp')
+left_iris_image = imread('~/Documents/Github/git_workspace/test/MMU-Iris-Database/3/left/chingycl1.bmp' )
+right_iris_image = imread('~/Documents/Github/git_workspace/test/MMU-Iris-Database/3/right/chingycr1.bmp'  )
 
-segmented_image = segmentation(iris_image)
+
+figure('Name','Iris Segmentation: Right eye','Position',[0 1000 700 500])
+right_resized_image = imresize(right_iris_image,2.5)
+right_segmented_image = segmentation(right_resized_image)
+
+
+
+figure('Name','Iris Segmentation: Left eye','Position',[800 1000 700 500])
+left_resized_image = imresize(left_iris_image,2.5)
+left_segmented_image = segmentation(left_resized_image)
+
 
--- Iris recognition system/segmentation.m
+++ Iris recognition system/segmentation.m
@@ -1,6 +1,8 @@
 function [segmented_image]= segmentation(iris_image)
 %binarization (converting grey scale image into black and white)
 
+
+% figure('Name','Iris Segmentation','Position',[50 1000 700 500])
 %displays the converted image RGB to gray image
 gray_image = rgb2gray(iris_image)
 subplot(2,3,1), imshow(gray_image, [])
@@ -17,7 +19,8 @@ subplot(2,3,3), imshow(canny_img,[])
 title('Canny edge')
 
 %plots the outer area and inner area of the pupil
-[outer_center,outer_radii] = imfindcircles(canny_img, [50,150],'Sensitivity',0.96);
+% [outer_center,outer_radii] = imfindcircles(canny_img, [50,150],'Sensitivity',0.96)
+[outer_center,outer_radii] = imfindcircles(canny_img, [100,300],'Sensitivity',0.96)
 disp(outer_center)
 disp(outer_radii)
 outer_center_value = outer_center(:,:);
@@ -27,7 +30,8 @@ viscircles(outer_center_value,outer_radii_value,'Edgecolor','b')
 title('Outer circle')
 
 %plots the inner area of the pupil
-[inner_center,inner_radii] = imfindcircles(canny_img, [10,40],'Sensitivity',0.8)
+% [inner_center,inner_radii] = imfindcircles(canny_img, [10,40],'Sensitivity',0.8)
+[inner_center,inner_radii] = imfindcircles(canny_img, [60,100],'Sensitivity',0.96)
 disp(inner_center)
 disp(inner_radii)
 inner_center_value = inner_center(:,:);
@@ -42,6 +46,10 @@ viscircles(inner_center_value,inner_radii_value,'Edgecolor','w')
 viscircles(outer_center_value,outer_radii_value,'Edgecolor','b')
 title('Outer and inner radius of iris')
 
+if (outer_center_value ~= 0) & (outer_radii_value ~=0) & (inner_center_value ~= 0) & (inner_radii_value ~=0)
+    segmented_image = 1
+else
+    segmented_image = 0
 %---------------------------------------------- alternative
 % binarized_image = imbinarize(iris_image,'adaptive','ForegroundPolarity','dark','Sensitivity',0.5)
 % canny_img = edge3(binarized_image,"approxcanny",0.6)
